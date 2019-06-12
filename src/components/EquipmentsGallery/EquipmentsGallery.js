ec58d46e83706f08705e652ef45d40e94d130957
Selvio Perez <selvio@alefeducation.com>
2019-06-12 21:32:14 +0400

    Gallery

--- src/components/EquipmentsGallery/EquipmentsGallery.js
+++ src/components/EquipmentsGallery/EquipmentsGallery.js
@@ -1,6 +1,51 @@
 import React from "react";
-import { MainContainer } from "./EquipmentsGalleryStyles";
+import { MainContainer, Card, Image, Item, Container } from "./EquipmentsGalleryStyles";
 
 export default function EquipmentsGallery() {
-  return <MainContainer>Gallery</MainContainer>;
+  return (
+    <MainContainer>
+      <Container>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+        <Item>
+          <Card>
+            <Image src={require("../../Images/montacargas.jpg")} alt="Montacargas" />
+          </Card>
+        </Item>
+      </Container>
+    </MainContainer>
+  );
 }
--- src/components/EquipmentsGallery/EquipmentsGalleryStyles.js
+++ src/components/EquipmentsGallery/EquipmentsGalleryStyles.js
@@ -2,5 +2,38 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   background: #e3e1e1;
+  display: flex;
+  justify-content: center;
   padding: 96px 72px 72px;
+`;
+
+export const Card = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  background: #fff;
+  height: 276px;
+  padding: 24px;
+  cursor: pointer;
+  border: 2px solid #fff;
+
+  &:hover {
+    border-color: #f57502;
+  }
+`;
+
+export const Image = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`;
+
+export const Item = styled.div`
+  padding: 12px;
+  width: 25%;
+  display: inline-block;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
 `;
